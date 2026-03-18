c93883f74689a911bf3d59ab3bcaef76be6f2e4f
Valeriia Zubrytska <valeriiaz@oddity.com>
2026-03-18 17:20:26 +0200

    finish task

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import "./App.module.css";
-import SearchBar from "./components/SearchBar/SearchBar";
-import fetchMovies from "./services/movieService";
-import type { Movie } from "./types/movie";
+import SearchBar from "../SearchBar/SearchBar";
+import fetchMovies from "../../services/movieService";
+import type { Movie } from "../../types/movie";
 import { Toaster, toast } from "react-hot-toast";
-import MovieGrid from "./components/MovieGrid/MovieGrid";
-import Loader from "./components/Loader/Loader";
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import MovieModal from "./components/MovieModal/MovieModal";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
